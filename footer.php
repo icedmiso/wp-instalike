ec300b1c4b5db724d8dc2f18a2710f5a7e3c9a68
Kat <67570152+icedmiso@users.noreply.github.com>
2020-06-29 22:49:12 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,21 @@
+           </div>
+           <div id="footer">
+			<script>
+			var scroll = new SmoothScroll('a[href*=#]', a[href*="javascript:window.scrollTo(0,0);"]');
+			</script>
+			   
+			   <div class="alignleftlink">
+				   &copy; <?php bloginfo('name'); ?> 2015-2019  
+				   <br>
+			   Powered by cup ramen and bubble tea. Also coffee. 
+			   
+			   </div>
+			   <div class="alignrightlink">
+				   <?php get_search_form(); ?>
+				   <a href="#">Go up ↑</a>
+			   </div>
+           </div>
+        </div>
+     <?php wp_footer(); ?>
+     </body>
+</html>
